48719336ec8d5d402d638861773d594248a2aeb0
somq14 <som.q14@gmail.com>
2020-06-27 15:07:55 +0900

    add: drawer navigation

--- src/components/containers/AppContainer.tsx
+++ src/components/containers/AppContainer.tsx
@@ -1,11 +1,23 @@
 import Page1 from 'src/components/pages/Page1'
 import Page2 from 'src/components/pages/Page2'
+import Page3 from 'src/components/pages/Page3'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 
-const AppContainer = createBottomTabNavigator({
+const TabContainer = createBottomTabNavigator({
   Tab1: Page1,
   Tab2: Page2,
+  Tab3: Page3,
 })
+
+const DrawerConatiner = createDrawerNavigator({
+  Main: TabContainer,
+  Tab1: Page1,
+  Tab2: Page2,
+  Tab3: Page3,
+})
+
+const AppContainer = DrawerConatiner
 
 export default createAppContainer(AppContainer)
